d6824abf6beec272921d02014afb93ce8e2923d6
skuhlmann <samkuhlmann@gmail.com>
2019-08-17 17:23:54 -0600

    remove deploy test to run job on its own

--- test/deploy.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { batchDeploy } = require("../jobs/batch-deploy");
-
-const test = async () => {
-  const res = await batchDeploy({ count: 5 });
-  console.log("res", res);
-};
-
-test();
